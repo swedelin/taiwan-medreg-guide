4717abf483cebccc08b40716841bfa664af3cffc
swedelin <126035408+swedelin@users.noreply.github.com>
2025-09-15 22:17:12 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/en" element={<En />} /> {/* ✅ 新增這行 */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
