8393460841be7a57bc104d3c007f0fcfed7a366f
swedelin <126035408+swedelin@users.noreply.github.com>
2025-09-15 21:26:31 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import En from "./pages/En"; // ✅ 新增這行
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
